b884dcb11e80de0737d7814e537071ab790cd051
yashrajyash <66232462+yashrajyash@users.noreply.github.com>
2021-06-26 17:50:23 +0530

    files initialized

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname+'/signup.html');
+});
+
+app.post('/', function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    console.log(firstName + lastName + email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fiels: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    
+    const url = 'https://us6.api.mailchimp.com/3.0/lists/a984f0f798';
+
+    const jsonData = JSON.stringify(data);
+
+    const options = {
+        method: 'POST',
+        auth: 'yash:1912e0fd57db1e8dbd220ef1b9152ece8-us6'
+    }
+
+    const request = https.request(url, options, function(response) {
+        
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname+'/success.html');
+        } else {
+            res.sendFile(__dirname+'/failure.html');
+        }
+        
+        response.on('data', function(data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+
+app.post('/failure', function(req, res) {
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server has started at http://localhost:');
+});
+
+
+// Api key
+// 912e0fd57db1e8dbd220ef1b9152ece8-us6
+// List id
+// a984f0f798
